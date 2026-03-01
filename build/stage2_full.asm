91af4b09598c0c0290b86787402fb21e979eb4f0
barsik0396 <Barsik0396@outlook.com>
2026-03-01 10:13:06 +0200

    Delete build/stage2_full.asm

--- build/stage2_full.asm
+++
@@ -1,9 +0,0 @@
-; BarsikOS v0.3 - Stage 2 Full System
-[BITS 16]
-[ORG 0x1000]
-
-%include "boot/stage2.asm"
-%include "ui/ui.asm"
-%include "installer/installer.asm"
-%include "kernel/kernel.asm"
-%include "dos/dos.asm"
